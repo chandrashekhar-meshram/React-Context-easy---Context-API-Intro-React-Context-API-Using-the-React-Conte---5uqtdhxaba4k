d1a326dce548508220e17e692bc576642ecc5647
CHANDRASHEKHAR MESHRAM <shekharonlinework@gmail.com>
2022-10-01 20:31:10 +0000

    Update 3 files

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
-
+const UserContext = createContext();
 const App = () => {
+    const [obj, setObj] = useState({name:"Newton", age:3})      
 
   return (
-    <div id="main">
-      
-        <UserProfile />
-      
+    <div id="main">        
+        <NameContext.Provider value={{obj, setObj}}>
+             <UserProfile />
+        </NameContext.Provider>
     </div>
   )
 }
-
 
 export default App;
 export {UserContext}
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,6 +3,15 @@ import React from 'react'
 import { UserContext } from "./App"
 
 const UserProfile = () =>{
+    const temp = useContext(UserContext);
+    
+    return(
+        <div>
+            <h1 id="name">Name - {temp.name} </h1>
+            <h1 id="age">Age - {temp.age} </h1>
+        </div>
+    )
+   
 
 }
 
